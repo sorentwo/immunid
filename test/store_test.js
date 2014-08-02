b4105e4a253a8b6f2d152c9e38fe5db6a3de7d13
Parker Selbert <parker@sorentwo.com>
2014-08-02 17:04:08 -0500

    Store event handling and additional functionality

--- test/store_test.js
+++ test/store_test.js
@@ -1,54 +1,95 @@
 var expect    = require('chai').expect;
+var sinon     = require('sinon');
 var Store     = require('../lib/store');
 var Immutable = require('immutable');
 
 describe('Store', function() {
-  describe('.add', function() {
+  var store;
+
+  beforeEach(function() {
+    store = new Store();
+  });
+
+  describe('#add', function() {
     it('replaces the entire store when values are added', function() {
-      var buckets = Store.buckets;
+      var buckets = store.buckets;
 
-      Store.add('tags', {id: 1});
+      store.add('tags', {id: 1});
 
-      expect(Store.buckets).not.to.eq(buckets);
+      expect(Immutable.is(buckets, store.buckets)).to.be.false;
+    });
+
+    it('broadcasts a change event to registered listeners', function() {
+      var listener = sinon.spy();
+
+      store.addChangeListener(listener);
+
+      store.add('tags', {id: 1});
+
+      expect(listener.called).to.be.true;
     });
   });
 
-  describe('.all', function() {
+  describe('#delete', function() {
+    it('removes an existing model from the bucket', function() {
+      store.add('tags', { id: 1 });
+
+      var buckets = store.buckets;
+
+      store.delete('tags', 1);
+
+      expect(store.find('tags', 1)).to.be.undefined;
+      expect(Immutable.is(buckets, store.buckets)).to.be.false;
+    });
+
+    it('broadcasts a change event', function() {
+      var listener = sinon.spy();
+
+      store.add('tags', { id: 1 });
+
+      store.addChangeListener(listener);
+
+      store.delete('tags', 1);
+
+      expect(listener.calledOnce).to.be.true;
+    });
+  });
+
+  describe('#all', function() {
     it('returns all models in the specified bucket', function() {
-      Store.add('tags', {id: 1});
+      store.add('tags', {id: 1});
 
-      expect(Store.all('tags').length).to.eq(1);
+      expect(store.all('tags').length).to.eq(1);
     });
   });
 
-  describe('.find', function() {
+  describe('#find', function() {
     it('returns the requested object by id', function() {
       var object = {id: 1};
 
-      Store.add('tags', object);
+      store.add('tags', object);
 
-      var found = Store.find('tags', 1);
+      var found = store.find('tags', 1);
 
       expect(found).to.eql(object);
     });
   });
 
-  describe('.some', function() {
+  describe('#some', function() {
     it('returns all of the models requested by id', function() {
-      Store.add('tags', {id: 1});
-      Store.add('tags', {id: 2});
+      store.add('tags', {id: 1});
+      store.add('tags', {id: 2});
 
-      var found = Store.some('tags', [1, 2]);
+      var found = store.some('tags', [1, 2]);
 
       expect(found.length).to.eq(2);
     });
 
     it('treats the same subset of models as equal', function() {
-      Store.add('tags', {id: 1});
-      Store.add('tags', {id: 2});
+      store.add('tags', {id: 1}).add('tags', {id: 2});
 
-      var foundA = Store.some('tags', [1, 2]);
-      var foundB = Store.some('tags', [1, 2]);
+      var foundA = store.some('tags', [1, 2]);
+      var foundB = store.some('tags', [1, 2]);
 
       expect(Immutable.is(foundA, foundB)).to.be.true;
     });
